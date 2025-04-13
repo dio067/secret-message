b7f17a8f4816e2e949f50b72922653fcbda840ee
dio067 <yousefnas06@gmail.com>
2025-04-13 04:48:32 +0400

    feat: display message with new link

--- src/script.js
+++ src/script.js
@@ -1,13 +1,24 @@
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
+const { hash } = window.location;
 
-    document.querySelector('#message-form').classList.add('hide');
-    document.querySelector('#link-form').classList.remove('hide');
-    const input = document.querySelector('#message-input');
-    const encrypted = btoa(input.value);
+const message = atob(hash.replace('#', ''));
 
-    const linkInput = document.querySelector('#link-input');
-    linkInput.value = `${window.location}#${encrypted}`;
-    linkInput.select();
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');	
+  document.querySelector('#message-show').classList.remove('hide');	
 
-})
+  document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
